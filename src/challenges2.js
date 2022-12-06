1e0f91087d3cfd59f9a31a98e0e061fa5e97c396
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2022-12-06 14:59:33 -0300

    resolvendo o desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,5 +1,38 @@
 // Desafio 11 - Crie a função generatePhoneNumber
 
+const generatePhoneNumber = (phoneArray) => {
+  let repetition = [];
+  let fourthNumber=[];
+  let lastNumbers = [];
+  if(phoneArray.length != 11){
+      return 'Array com tamanho incorreto';
+  } else {
+      for(let index = 0; index < phoneArray.length; index +=1 ){
+          repetition[index] = 0;
+          if(phoneArray[index] < 0 || phoneArray[index] > 9){
+              return `não é possível gerar um número de telefone com esses valores`;
+          }
+          for(let count = 0; count< phoneArray.length; count+=1){
+              if(phoneArray[index]=== phoneArray[count]){
+                  repetition[index] +=1;
+              }
+          }
+      }
+  }
+  for (let index = 0; index < repetition.length; index+=1){
+      if(repetition[index]>=3){
+          return `não é possível gerar um número de telefone com esses valores`;
+      }
+  }
+  for (let index = 2; index < 6; index+=1){
+      fourthNumber.push(phoneArray[index]);
+  }
+  for (let index = 6; index < 10; index+=1){
+      lastNumbers.push(phoneArray[index]);
+  }
+return `(${phoneArray[0]}${phoneArray[1]}) ${fourthNumber.join('')}-${lastNumbers.join('')}`
+}
+
 // Desafio 12 -  Crie a função triangleCheck
 
 // Desafio 13 - Crie a função hydrate
